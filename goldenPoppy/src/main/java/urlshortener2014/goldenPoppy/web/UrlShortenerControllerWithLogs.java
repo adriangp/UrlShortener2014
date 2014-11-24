2b9f90bd58042b5ebb9bd8cd3a2bb4ab511057dc
adriangp <adrian.ganan.prado@gmail.com>
2014-11-24 21:21:57 +0100

    Demo project cloned

---
+++ goldenPoppy/src/main/java/urlshortener2014/goldenPoppy/web/UrlShortenerControllerWithLogs.java
@@ -0,0 +1,34 @@
+package urlshortener2014.goldenPoppy.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import urlshortener2014.common.domain.ShortURL;
+import urlshortener2014.common.web.UrlShortenerController;
+
+@RestController
+public class UrlShortenerControllerWithLogs extends UrlShortenerController {
+
+	private static final Logger logger = LoggerFactory.getLogger(UrlShortenerControllerWithLogs.class);
+	
+	@Override
+	public ResponseEntity<?> redirectTo(@PathVariable String id) {
+		logger.info("Requested redirection with hash "+id);
+		return super.redirectTo(id);
+	}
+
+	@Override
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<ShortURL> shortener(
+			@RequestParam MultiValueMap<String, String> form) {
+		logger.info("Requested new short for uri "+form.getFirst("url"));
+		return super.shortener(form);
+	}
+}
