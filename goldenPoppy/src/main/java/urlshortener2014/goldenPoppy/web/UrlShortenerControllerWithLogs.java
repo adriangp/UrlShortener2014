a771b81f25ff754b573667c9d851ee989f926611
adriangp <adrian.ganan.prado@gmail.com>
2014-12-14 13:15:11 +0100

    GoldenPoppy adapted to core changes

--- goldenPoppy/src/main/java/urlshortener2014/goldenPoppy/web/UrlShortenerControllerWithLogs.java
+++ goldenPoppy/src/main/java/urlshortener2014/goldenPoppy/web/UrlShortenerControllerWithLogs.java
@@ -1,12 +1,11 @@
 package urlshortener2014.goldenPoppy.web;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,17 +17,18 @@ public class UrlShortenerControllerWithLogs extends UrlShortenerController {
 
 	private static final Logger logger = LoggerFactory.getLogger(UrlShortenerControllerWithLogs.class);
 	
-	@Override
-	public ResponseEntity<?> redirectTo(@PathVariable String id) {
+	public ResponseEntity<?> redirectTo(@PathVariable String id, 
+			HttpServletRequest request) {
 		logger.info("Requested redirection with hash "+id);
-		return super.redirectTo(id);
+		return super.redirectTo(id, request);
 	}
 
-	@Override
-	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<ShortURL> shortener(
-			@RequestParam MultiValueMap<String, String> form) {
-		logger.info("Requested new short for uri "+form.getFirst("url"));
-		return super.shortener(form);
+	public ResponseEntity<ShortURL> shortener(@RequestParam("url") String url,
+			@RequestParam(value = "sponsor", required = false) String sponsor,
+			@RequestParam(value = "brand", required = false) String brand,
+			HttpServletRequest request) {
+		logger.info("Requested new short for uri "+url);
+		return super.shortener(url, sponsor, brand, request);
 	}
 }
+
