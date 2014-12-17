d3f4c69198632c40288577734dde1f66a3aa8487
Jorge Izquierdo Bueno <jorgeizquierdobueno@gmail.com>
2014-12-17 22:28:18 +0100

    Trying Spring's Web Socket

---
+++ oldBurgundy/src/main/java/urlshortener2014/oldBurgundy/web/rest/validator/Url.java
@@ -0,0 +1,24 @@
+package urlshortener2014.oldBurgundy.web.rest.validator;
+
+public class Url {
+	
+	private String url;
+	private int protocol;
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public int getProtocol() {
+		return protocol;
+	}
+	
+	public void setProtocol(int protocol) {
+		this.protocol = protocol;
+	}
+	
+}
