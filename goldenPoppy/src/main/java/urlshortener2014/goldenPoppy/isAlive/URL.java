7f84173150574ab0b7318fd844d8978d27692056
Javier Tello <jtelloal@gmail.com>
2014-12-19 23:05:55 +0100

    Building isAlive service

---
+++ goldenPoppy/src/main/java/urlshortener2014/goldenPoppy/isAlive/URL.java
@@ -0,0 +1,15 @@
+package urlshortener2014.goldenPoppy.isAlive;
+
+public class URL {
+	
+	private String url;
+	
+	public URL(String url){
+		this.url = url;
+	}
+	
+	public String getUrl(){
+		return url;
+	}
+	
+}
