488eebab395bd799c634ee337b1218db11544a3e
Javier Tello <jtelloal@gmail.com>
2014-12-21 19:37:55 +0100

    HTML isAlive created

--- goldenPoppy/src/main/java/urlshortener2014/goldenPoppy/web/UrlShortenerControllerWithLogs.java
+++ goldenPoppy/src/main/java/urlshortener2014/goldenPoppy/web/UrlShortenerControllerWithLogs.java
@@ -2,11 +2,19 @@ package urlshortener2014.goldenPoppy.web;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +22,7 @@ import urlshortener2014.common.domain.ShortURL;
 import urlshortener2014.common.repository.ShortURLRepository;
 import urlshortener2014.common.web.UrlShortenerController;
 import urlshortener2014.goldenPoppy.intesicial.IntersicialEndPoint;
+import urlshortener2014.goldenPoppy.isAlive.*;
 
 @RestController
 public class UrlShortenerControllerWithLogs extends UrlShortenerController {
@@ -56,4 +65,21 @@ public class UrlShortenerControllerWithLogs extends UrlShortenerController {
 			HttpServletRequest request){
 		return shortener(sUrl,sponsor,null,request);
 	}
+	
+	//@RequestMapping(value = "/isalive", method = RequestMethod.POST)
+	@MessageMapping("/isalive")
+    @SendTo("/topic/isalive")
+    public Response isalive(URL url) throws Exception {
+    	// TODO Implementar el timer
+    	
+    	HttpClient client = HttpClientBuilder.create().build();
+    	HttpHead request = new HttpHead(url.getUrl());
+    	HttpResponse response = client.execute(request);
+    	
+    	int resultado = response.getStatusLine().getStatusCode();
+    	String phrase = response.getStatusLine().getReasonPhrase();
+    	
+    	
+        return new Response(resultado + " " + phrase);
+    }
 }
