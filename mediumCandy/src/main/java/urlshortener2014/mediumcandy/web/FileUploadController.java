9f82a41bf0e02265db06a472555a47b26fccca83
Carlos Martínez <carlosmartcast@gmail.com>
2015-01-05 16:31:28 +0100

    adapted post method
    as frontend only allows one file per post request, IF clause where WHILE
    was

--- mediumCandy/src/main/java/urlshortener2014/mediumcandy/web/FileUploadController.java
+++ mediumCandy/src/main/java/urlshortener2014/mediumcandy/web/FileUploadController.java
@@ -20,48 +20,32 @@ public class FileUploadController {
     public @ResponseBody String provideUploadInfo() {
         return "You can upload a file by posting to this same URL.";
     }
-
-    @RequestMapping(value="/upload2", method=RequestMethod.POST)
-    public @ResponseBody String handleFileUpload(MultipartFile file){
-    	String name = "file.csv";
-    	System.out.println("\nEJECUTANDOOOOOOOOOOOO!\n");
-    	if (!file.isEmpty()) {
-            try {
-                byte[] bytes = file.getBytes();
-                BufferedOutputStream stream =
-                        new BufferedOutputStream(new FileOutputStream(new File(name)));
-                stream.write(bytes);
-                stream.close();
-                return "You successfully uploaded " + name + "!";
-            } catch (Exception e) {
-                return "You failed to upload " + name + " => " + e.getMessage();
-            }
-        } else {
-        	System.out.println("\nEMPTY FILE!\n");
-            return "You failed to upload " + name + " because the file was empty.";                    
-        }
-    }
     
     @RequestMapping(value="/upload", method=RequestMethod.POST)
     public @ResponseBody String handleFileUpload(MultipartHttpServletRequest request){
         Iterator<String> iterator = request.getFileNames();
-        while (iterator.hasNext()) {
+        if ( iterator.hasNext() ) {
             String fileName = iterator.next();
             MultipartFile multipartFile = request.getFile(fileName);
-            try {
-				byte[] file = multipartFile.getBytes();
-				BufferedOutputStream stream =
-                        new BufferedOutputStream(new FileOutputStream(new File("name.csv")));
-                stream.write(file);
-                stream.close();
-                return "You successfully uploaded !";
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				return "You failed to upload  => " + e.getMessage();
-			}            
+            
+            if ( !multipartFile.isEmpty() ) {
+            	try {
+    				byte[] file = multipartFile.getBytes();
+    				BufferedOutputStream stream =
+                            new BufferedOutputStream(new FileOutputStream(new File("name.csv")));
+                    stream.write(file);
+                    stream.close();
+                    return "You successfully uploaded !";
+    			} catch (IOException e) {
+    				// TODO Auto-generated catch block
+    				return "You failed to upload  => " + e.getMessage();
+    			}
+            } else {
+            	return "You failed to upload => EMPTY FILE!";
+            }
         }
         
-        return "You failed to upload because the file was empty."; 
+        return "No files found in request!"; 
     }
 
 }
