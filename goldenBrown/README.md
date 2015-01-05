06847c739c1516a262265d58bd4bcbcc7635c16f
JorgeGaruz <sdfgenki@hotmail.com>
2015-01-06 00:00:49 +0100

    Anadido el readme con las funcionalidad y mejora del javadoc

--- goldenBrown/README.md
+++ goldenBrown/README.md
@@ -6,6 +6,18 @@ Golden brown is commonly referenced in recipes as the desired color of properly 
 
 # Team Members
 
-* Javier García Barcos (leader)
-* Jorge Garuz Sánchez
+* Javier Garcia Barcos (leader)
+* Jorge Garuz Sanchez
 * Gabriel Barreras Sanz
+
+# Functionalities
+### URL Reachable(totally implemented)
+Web service that consist of prove if an URL can be reachable using the HEAD petition to the URL and avoid redirections. Also the Web Service has a Time Out, after that if the page isn't reachable the answer is a Not Found HTTP.
+
+### Platform and navigator identifier(totally implemented)
+Web service that consist of read the headers User-Agent of the request HTTP and about it, clasify navigator and platform of the click. For that, we use the library UserAgentUtils for fill better information of the request.
+
+### BlackList (totally implemented)
+Web service that use third parties BD for prove the IP of the URL to shortener. This option is rebuilt from a PHP program. Also, we use a cache for avoid bomb the service if the IP's are know.
+
+### Publicity interstice (NO implemented)
--- goldenBrown/src/main/java/urlshortener2014/goldenbrown/platformidentifier/PlatformIdentity.java
+++ goldenBrown/src/main/java/urlshortener2014/goldenbrown/platformidentifier/PlatformIdentity.java
@@ -2,11 +2,17 @@ package urlshortener2014.goldenbrown.platformidentifier;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import nl.bitwalker.useragentutils.Browser;
-import nl.bitwalker.useragentutils.OperatingSystem;
+import nl.bitwalker.useragentutils.Browser;//TODO REMOVE
+import nl.bitwalker.useragentutils.OperatingSystem;//TODO REMOVE
 import nl.bitwalker.useragentutils.UserAgent;
-import nl.bitwalker.useragentutils.Version;
-
+import nl.bitwalker.useragentutils.Version;//TODO REMOVE
+/**
+ * Web service that consist of read the headers User-Agent of the request HTTP and about it,
+ * clasify navigator and platform of the click. For that, we use the library UserAgentUtils 
+ * for fill better information of the request.
+ * @author: Jorge,Javi,Gabi
+ * @version: 08/01/2015
+ */
 public class PlatformIdentity {
 	private UserAgent agent;
 	@JsonProperty("browser")
@@ -15,10 +21,18 @@ public class PlatformIdentity {
 	private String version;
 	@JsonProperty("os")
 	private String os;
-	
-	// Dummy constructor needed to be Jackson-parseable
+
+	/**
+	 * Dummy constructor needed to be Jackson-parseable
+	 */
 	public PlatformIdentity(){}
 	
+	/**
+	 * Method that uses the class UserAgent from UserAgentUtils package identifier the navigator
+	 * and the platform of the user.
+	 * @param us contain the User-Agent
+	 * @throws IllegalArgumentException
+	 */
 	public PlatformIdentity(String us) throws IllegalArgumentException{
 		if (us != null){
 			this.agent = UserAgent.parseUserAgentString(us);
@@ -35,17 +49,23 @@ public class PlatformIdentity {
 			new IllegalArgumentException("User-Agent cannot be null.");
 		}
 	}
-	
+	/**
+	 * @return the browser information
+	 */
 	@JsonProperty("browser")
 	public String getBrowser() {
 		return this.browser;
 	}
-	
+	/**
+	 * @return the version information
+	 */
 	@JsonProperty("version")
 	public String getVersion() {
 		return this.version;
 	}
-	
+	/**
+	 * @return the OS information
+	 */
 	@JsonProperty("os")
 	public String getOs() {
 		return this.os;
