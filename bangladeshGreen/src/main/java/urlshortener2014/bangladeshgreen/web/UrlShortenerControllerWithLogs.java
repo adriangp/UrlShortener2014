6dbf508d722c55c109c2a96bff8d671ff1aef764
alberto-648702 <alberto_blsc@hotmail.com>
2015-01-06 09:55:02 +0100

    Interfaz web casi casi

--- bangladeshGreen/src/main/java/urlshortener2014/bangladeshgreen/web/UrlShortenerControllerWithLogs.java
+++ bangladeshGreen/src/main/java/urlshortener2014/bangladeshgreen/web/UrlShortenerControllerWithLogs.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.sql.Date;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -40,161 +39,170 @@ public class UrlShortenerControllerWithLogs extends UrlShortenerController {
 	@Autowired
 	private ClickRepository clickRepository;
 	private ShortURLRepository SURLR;
-	private static final Logger logger = LoggerFactory.getLogger(UrlShortenerControllerWithLogs.class);
-	
-	
-	@RequestMapping(value = "/Upload", method = RequestMethod.POST)
-	public String login(HttpServletRequest request) throws Exception{
-		Part part = request.getPart("fileToUpload");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(part.getInputStream()));
-		
-    	File fichero=new File("fichero.csv");
-    	fichero.createNewFile();
-    	PrintWriter f = new PrintWriter(fichero);
-    	String linea="";
-    	while((linea = reader.readLine()) != null){
-    		System.out.println(linea);
-    		if(linea.startsWith("www") || linea.startsWith("http")){
-    			String []listaURL=linea.split(",");
-				for(String url: listaURL){
-					//if(!url.isEmpty() || !url.equals(null))
-						f.write(url+",");
-				}
-				f.write("\n");
-    		}
-		}
-    	f.close();
-    	analizarCSV(fichero,request);
-		return "File Uploaded!";
-	}
-	
-	public ResponseEntity<?> redirectTo(@PathVariable String id, HttpServletRequest request) {
-		logger.info("Requested redirection with hash "+id);
+	private static final Logger logger = LoggerFactory
+			.getLogger(UrlShortenerControllerWithLogs.class);
+
+	public ResponseEntity<?> redirectTo(@PathVariable String id,
+			HttpServletRequest request) {
+		logger.info("Requested redirection with hash " + id);
 		String agent = request.getHeader("User-Agent");
-		String ip=request.getRemoteAddr();
-		String navegador="",SO="";
-		if(agent.indexOf("Chrome")!=-1) navegador="Chrome";
-		else if(agent.indexOf("Firefox")!=-1) navegador="Firefox";
-		else if(agent.indexOf("Safari")!=-1) navegador="Safari";
-		else navegador="Explorer";
-		
-		if(agent.indexOf("Windows")!=-1) SO="Windows";
-		else if(agent.indexOf("Linux")!=-1) SO="Linux";
-		else if(agent.indexOf("Macintosh")!=-1) SO="Macintosh";
-	
-		logger.info("Requested redirection with hash "+id);
-		// Guardar en un objeto la llamada al padre, guardarme en una lista la consulta
-		// a los Cliks, y quedarme con el ultimo con la IP del request, modificar el
+		String ip = request.getRemoteAddr();
+		String navegador = "", SO = "";
+		if (agent.indexOf("Chrome") != -1)
+			navegador = "Chrome";
+		else if (agent.indexOf("Firefox") != -1)
+			navegador = "Firefox";
+		else if (agent.indexOf("Safari") != -1)
+			navegador = "Safari";
+		else
+			navegador = "Explorer";
+
+		if (agent.indexOf("Windows") != -1)
+			SO = "Windows";
+		else if (agent.indexOf("Linux") != -1)
+			SO = "Linux";
+		else if (agent.indexOf("Macintosh") != -1)
+			SO = "Macintosh";
+
+		logger.info("Requested redirection with hash " + id);
+		// Guardar en un objeto la llamada al padre, guardarme en una lista la
+		// consulta
+		// a los Cliks, y quedarme con el ultimo con la IP del request,
+		// modificar el
 		// click con el navegador y SO, actualizar BD y return
-		ResponseEntity<?> response=super.redirectTo(id,request);
-		List<Click> listaClicks=clickRepository.findByHash(id);
-		logger.info("Tamaño: "+listaClicks.size()+"ip: "+ip);
-		for(int i=listaClicks.size()-1;i>=0;i--){
-			Click click=listaClicks.get(i);
-			logger.info("Click con ip: "+click.getIp());
-			if(click.getIp().equals(ip)){
-				String hash=click.getHash();
-				Long identificador=click.getId();
-				Date fecha=click.getCreated();
-				Click clickFinal=new Click(identificador,hash,fecha,null,navegador,SO,ip,null);
+		ResponseEntity<?> response = super.redirectTo(id, request);
+		List<Click> listaClicks = clickRepository.findByHash(id);
+		logger.info("Tamaño: " + listaClicks.size() + "ip: " + ip);
+		for (int i = listaClicks.size() - 1; i >= 0; i--) {
+			Click click = listaClicks.get(i);
+			logger.info("Click con ip: " + click.getIp());
+			if (click.getIp().equals(ip)) {
+				String hash = click.getHash();
+				Long identificador = click.getId();
+				Date fecha = click.getCreated();
+				Click clickFinal = new Click(identificador, hash, fecha, null,
+						navegador, SO, ip, null);
 				clickRepository.update(clickFinal);
-				logger.info("Actualizado Click con "+navegador+"SO: "+SO+" e ip:"+ip);
+				logger.info("Actualizado Click con " + navegador + "SO: " + SO
+						+ " e ip:" + ip);
 				break;
 			}
 		}
-	
+
 		return response;
 	}
 
-	
 	public ResponseEntity<ShortURL> shortener(@RequestParam("url") String url,
 			@RequestParam(value = "sponsor", required = false) String sponsor,
 			@RequestParam(value = "brand", required = false) String brand,
 			HttpServletRequest request) {
-		
-		logger.info("Requested new short for uri "+"url");
-		
-		ResponseEntity<ShortURL>su=super.shortener(url, sponsor, brand, request);
-		
-		//comprobar si es segura
+
+		logger.info("Requested new short for uri " + "url");
+
+		ResponseEntity<ShortURL> su = super.shortener(url, sponsor, brand,
+				request);
+
+		// comprobar si es segura
 		Client c = ClientBuilder.newClient();
-		url=parse(url);
+		url = parse(url);
 		Response response = c
-				.target("https://sb-ssl.google.com/safebrowsing/api/lookup?client=Roberto&key=AIzaSyBbjDCPwK13dOYioVf6Cp9_lrFZ_MOEFbU&appver=1.5.2&pver=3.1&url="+url)
-				.request(MediaType.TEXT_HTML)
-				.get();
-		
-		if(response.getStatus()==200)
-			SURLR.mark(su.getBody(), false);//marcar como no segura
+				.target("https://sb-ssl.google.com/safebrowsing/api/lookup?client=Roberto&key=AIzaSyBbjDCPwK13dOYioVf6Cp9_lrFZ_MOEFbU&appver=1.5.2&pver=3.1&url="
+						+ url).request(MediaType.TEXT_HTML).get();
+
+		if (response.getStatus() == 200)
+			SURLR.mark(su.getBody(), false);// marcar como no segura
 
 		return su;
 	}
 
-	public Response analizarCSV(File csv,
-			HttpServletRequest request) throws IOException{
-		
-		BufferedReader br=new BufferedReader(new FileReader(csv));
-		File csvAcortado=new File("fich_temporal.csv");
-		PrintWriter fileResul=new PrintWriter(csvAcortado);
-		String linea="";
-		while((linea = br.readLine()) != null){
-				String []listaURL=linea.split(",");
-				for(String url: listaURL){
-					System.out.println("Url "+url);
-					if(!url.equals(null) || !url.equals("")){
-						ResponseEntity<ShortURL> urlAcortada=shortener(url,null,null,request);
-						fileResul.write(urlAcortada.getBody().getUri().toString()+",");
-					}
+	@RequestMapping(value = "/Upload", method = RequestMethod.POST)
+	public String upload(HttpServletRequest request) throws Exception {
+		Part part = request.getPart("fileToUpload");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				part.getInputStream()));
+
+		File fichero = new File("src\\main\\resources\\public\\csv\\fich_original.csv");
+		fichero.createNewFile();
+		PrintWriter f = new PrintWriter(fichero);
+		String linea = "";
+		while ((linea = reader.readLine()) != null) {
+			System.out.println(linea);
+			String[] listaURL = linea.split(",");
+			for (String url : listaURL) 
+				f.write(url + ",");
+			f.write("\n");
+		}
+		f.close();
+		analizarCSV(fichero, request);
+		return "File Uploaded!";
+	}
+
+	public Response analizarCSV(File csv, HttpServletRequest request)
+			throws IOException {
+
+		BufferedReader br = new BufferedReader(new FileReader(csv));
+		File csvAcortado = new File("src\\main\\resources\\public\\csv\\fich_temporal.csv");
+		csvAcortado.createNewFile();
+		PrintWriter fileResul = new PrintWriter(csvAcortado);
+		String linea = "";
+		while ((linea = br.readLine()) != null) {
+			String[] listaURL = linea.split(",");
+			for (String url : listaURL) {
+				System.out.println("Url " + url);
+				if (!url.equals(null) || !url.equals("")) {
+					ResponseEntity<ShortURL> urlAcortada = shortener(url, null,
+							null, request);
+					fileResul.write(urlAcortada.getBody().getUri().toString()
+							+ ",");
 				}
-				fileResul.write("\n");
+			}
+			fileResul.write("\n");
 		}
 		fileResul.close();
 		br.close();
 		return Response.status(Status.OK).build();
 	}
-	
-	
-	
+
 	private static String parse(String a) {
-		String res="";
-		for(int i=0;i<a.length();i++){
-			switch(a.charAt(i)){
-				case ':':
-					res=res+"%3A";
-					break;
-				case '/':
-					res=res+"%2F";
-					break;
-				case ' ':
-					res=res+"%20";
-					break;
-				case '?':
-					res=res+"%3F";
-					break;
-				case '<':
-					res=res+"%3C";
-					break;
-				case '>':
-					res=res+"%3E";
-					break;
-				case '%':
-					res=res+"%25";
-					break;
-				case '#':
-					res=res+"%23";
-					break;
-				case ';':
-					res=res+"%3B";
-					break;
-				case '|':
-					res=res+"%7C";
-					break;
-				case '&':
-					res=res+"%26";
-					break;
-				default:
-					res=res+a.charAt(i);
-					break;
+		String res = "";
+		for (int i = 0; i < a.length(); i++) {
+			switch (a.charAt(i)) {
+			case ':':
+				res = res + "%3A";
+				break;
+			case '/':
+				res = res + "%2F";
+				break;
+			case ' ':
+				res = res + "%20";
+				break;
+			case '?':
+				res = res + "%3F";
+				break;
+			case '<':
+				res = res + "%3C";
+				break;
+			case '>':
+				res = res + "%3E";
+				break;
+			case '%':
+				res = res + "%25";
+				break;
+			case '#':
+				res = res + "%23";
+				break;
+			case ';':
+				res = res + "%3B";
+				break;
+			case '|':
+				res = res + "%7C";
+				break;
+			case '&':
+				res = res + "%26";
+				break;
+			default:
+				res = res + a.charAt(i);
+				break;
 			}
 		}
 		return res;
