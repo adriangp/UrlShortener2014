6379207f95e52a18c0c040a29ea42817fb9f0de5
Pol <poles91@gmail.com>
2015-01-06 22:05:25 +0100

    part2

--- dimGray/src/main/resources/public/js/showData.js
+++ dimGray/src/main/resources/public/js/showData.js
@@ -9,13 +9,26 @@ $(document).ready(function() {
 					$("#modify").html("<table class='table table-bordered' id='infoTable'> <tr> <td>Mod</td><td>Del</td><td>Url</td><td>Uri-Id</td><td>Sponsor-Created</td><td>Created-Referrer</td><td>Owner-Browser</td><td>Mode-Platform</td><td>Ip</td><td>Country</td></tr></table>");
 					
 					$.each(message, function(index, value) {
+						
 						if (value.isUrl == 1) {
-							var val = "url "+value.hash;
-							$("#infoTable").append('<tr><td><input id="mod" type="radio" name="radMod" value="'+val+'"></td><td><input id="mod" type="radio" name="radDel" value="'+val+'"></td><td>'+value.target+'</td><td>'+value.uri+'</td><td>'+value.sponsor+'</td><td>'+value.urlCreated+'</td><td>'+value.owner+'</td><td>'+value.mode+'</td><td>'+value.urlIp+'</td><td>'+value.urlCountry+'</td></tr>');
+							var hash = value.hash, target = value.target, uri = value.uri, sponsor = value.sponsor, created = value.urlCreated, owner = value.owner, mode = value.mode, ip = value.urlIp, country = value.urlCountry;
+							
+							if (uri == null)	uri = "-";
+							if (sponsor == null)	sponsor = "-";
+							if (owner == null)	owner = "-";
+							if (country == null)	country = "-";
+							
+							var val = "url "+hash;
+							$("#infoTable").append('<tr><td><input id="mod" type="radio" name="radMod" value="'+val+'"></td><td><input id="mod" type="radio" name="radDel" value="'+val+'"></td><td>'+target+'</td><td>'+uri+'</td><td>'+sponsor+'</td><td>'+created+'</td><td>'+owner+'</td><td>'+mode+'</td><td>'+ip+'</td><td>'+country+'</td></tr>');
 						}
 						else  {
-							var val = "click "+value.id+" "+value.hash+" "+value.clickCreated+" "+value.referrer+" "+value.browser+" "+value.platform+" "+value.clickIp+" "+value.clickCountry;
-							$("#infoTable").append('<tr><td><input type="radio" name="radMod" value="'+val+'"></td><td><input type="radio" name="radDel" value="'+val+'"></td><td></td><td>'+value.id+'</td><td>'+value.clickCreated+'</td><td>'+value.referrer+'</td><td>'+value.browser+'</td><td>'+value.platform+'</td><td>'+value.clickIp+'</td><td>'+value.clickCountry+'</td><td></td></tr>');
+							var id = value.id, hash = value.hash, created = value.clickCreated, referrer = value.referrer, browser = value.browser, platform = value.platform, ip = value.clickIp, country = value.clickCountry;
+							var val = "click "+id+" "+hash+" "+created+" "+referrer+" "+browser+" "+platform+" "+ip+" "+country;
+							if (referrer == null)	referrer = "-";
+							if (browser == null)	browser = "-";
+							if (platform == null)	platform = "-";
+							if (country == null)	country = "-";
+							$("#infoTable").append('<tr><td><input type="radio" name="radMod" value="'+val+'"></td><td><input type="radio" name="radDel" value="'+val+'"></td><td></td><td>'+id+'</td><td>'+created+'</td><td>'+referrer+'</td><td>'+browser+'</td><td>'+platform+'</td><td>'+ip+'</td><td>'+country+'</td><td></td></tr>');
 						}
 					});
 					
